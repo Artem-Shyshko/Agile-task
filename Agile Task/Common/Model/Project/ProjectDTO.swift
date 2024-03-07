e0b0351f1c7f45232582031f27c417657d3c7159
Artur Korol <korolarthur7@gmail.com>
2024-03-07 15:02:07 +0200

    feat: Updated project name

--- Agile Task/Common/Model/Project/ProjectDTO.swift
+++ Agile Task/Common/Model/Project/ProjectDTO.swift
@@ -28,7 +28,7 @@ extension ProjectDTO {
     static func mockProject() -> ProjectDTO {
         ProjectDTO(
             id: ObjectId.generate(),
-            name: "General",
+            name: "Default project",
             isSelected: true,
             tasks: TaskDTO.mockArray()
         )
