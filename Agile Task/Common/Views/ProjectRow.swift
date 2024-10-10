4361e2cb825fc68b9b2e98fa0dfaeb9f4353ae11
Alexandra <user@USERs-MacBook-Pro.local>
2024-10-10 14:03:59 +0300

    Add edit button to list, fix default task naming

--- Agile Task/Common/Views/ProjectRow.swift
+++ Agile Task/Common/Views/ProjectRow.swift
@@ -14,17 +14,33 @@ struct ProjectRow: View {
     @StateObject var vm: ProjectsViewModel
     var project: ProjectDTO
     @State var isShowingDeleteAlert = false
+    @Binding var path: [ProjectNavigation]
     
     var body: some View {
         Button {
             vm.selectProject(project)
         } label: {
-            HStack(spacing: 5) {
-                if project.isSelected { checkMark }
-                
-                Text(project.name)
-                    .font(.helveticaRegular(size: 16))
-                    .foregroundStyle(themeManager.theme.sectionTextColor(colorScheme))
+            GeometryReader { geometry in
+                HStack(alignment: .center, spacing: 5) {
+                    if project.isSelected { checkMark }
+                    
+                    Text(project.name)
+                        .font(.helveticaRegular(size: 16))
+                        .foregroundStyle(themeManager.theme.sectionTextColor(colorScheme))
+                    Spacer()
+                }
+                .frame(width: geometry.size.width)
+                .padding(.top, 8)
+                .overlay(alignment: .trailingLastTextBaseline, content: {
+                    Button {
+                        path.append(.newProject(editHabit: project))
+                    } label: {
+                        Image(.swipes)
+                            .frame(size: 20)
+                    }
+                    .offset(x: setOffsetForSwipesButton(), y: 4)
+                    .buttonStyle(.borderless)
+                })
             }
         }
         .swipeActions {
@@ -52,15 +68,36 @@ struct ProjectRow: View {
             }
         }
         .padding(.horizontal, Constants.shared.listRowHorizontalPadding)
+//        .listRowBackground(
+//            RoundedRectangle(cornerRadius: 4)
+//                .fill(themeManager.theme.sectionColor(colorScheme))
+//                .padding(.trailing, 10)
+//            .overlay(alignment: .trailing   , content: {
+//                Image(.swipes)
+//                    .onTapGesture {
+//                        path.append(.newProject(editHabit: project))
+//                    }
+//            })
+//        )
+//        .padding(.trailing, 10)
+        
         .listRowBackground(
             RoundedRectangle(cornerRadius: 4)
                 .fill(themeManager.theme.sectionColor(colorScheme))
-                .padding(.trailing, 10)
-            .overlay(alignment: .trailing   , content: {
-                Image(.swipes)
-            })
+                .padding(.trailing, 12)
         )
-        .padding(.trailing, 10)
+        .padding(.trailing, 12)
+//        .overlay(alignment: .trailing, content: {
+//            Button {
+////                path.append(.createTask(editedTask: task))
+//            } label: {
+//                Image(.swipes)
+//                    .padding(.trailing, 2)
+//                    .frame(size: 20)
+//            }
+////            .offset(x: 25)
+//            .buttonStyle(.borderless)
+//        })
     }
     
     var checkMark: some View {
@@ -70,5 +107,11 @@ struct ProjectRow: View {
             .scaledToFit()
             .frame(width: 15, height: 15)
             .foregroundStyle(themeManager.theme.sectionTextColor(colorScheme))
+    }
+    
+    func setOffsetForSwipesButton() -> CGFloat {
+        let screenWidth = UIScreen.main.bounds.size.width
+        // different value for iPads and phones with smaller width
+        return (screenWidth < 744 && screenWidth > 400) ? 26 : 21.5
     }
 }
